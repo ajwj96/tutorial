a2e8b9e96eb4eed4333ac47e3a614b9b1a78ce70
EC2 Default User <ec2-user@ip-172-31-30-58.us-east-2.compute.internal>
2019-04-06 02:33:44 +0000

    コードを整理

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -12,19 +12,20 @@ class FolderController extends Controller
     {
         return view('folders/create');
     }
-public function create(CreateFolder $request) // ★ 引数の型を変更
-{
-    // フォルダモデルのインスタンスを作成する
-    $folder = new Folder();
-    // タイトルに入力値を代入する
-    $folder->title = $request->title;
-    // インスタンスの状態をデータベースに書き込む
-    $folder->save();
-
-    return redirect()->route('tasks.index', [
-        'id' => $folder->id,
-    ]);
-}
+    
+    public function create(CreateFolder $request) // ★ 引数の型を変更
+    {
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+        // インスタンスの状態をデータベースに書き込む
+        $folder->save();
+    
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
 }
 
 
